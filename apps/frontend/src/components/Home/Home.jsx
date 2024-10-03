2f4c1461834bb2eed37d0a525fcbcac6d68bff38
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-10-03 23:21:49 +0530

    Files Setup

--- apps/frontend/src/components/Home/Home.jsx
+++ apps/frontend/src/components/Home/Home.jsx
@@ -1,32 +1,13 @@
-import background from "../../assets/background.mp4"
-import  blueP  from '../../assets/blueplanet.svg';
-export default function Home() {
+// import React from 'react'
+
+const Home = () => {
   return (
-    <div>
-       <div className="relative h-screen w-full">
-      <video
-        className="absolute top-0 mr-4 w-full h-full object-cover"
-        autoPlay
-        loop
-        muted
-      >
-        <source src={background} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-
-     
-      <div className="relative flex items-start ">
-        {/* <h1 className="text-white text-7xl font-bold mt-[23vh] ml-[25vh]">
-          Welcome to<br/>
-          our
-        </h1> */}
-        <div className="fixed top-10 left-10">
-          <img  className="w-full h-full mt-[23vh] md:block " src={blueP} />  {/* Adjust size and positioning */}
-        </div>
-      </div>
-    </div>
+    <div className="text-4xl text-blue-500">
+      <h1 className="text-4xl text-rose-500">
+        Hey Puskar This Side
+      </h1>
     </div>
   )
 }
+
+export default Home
