f97698570c3a0cfaf7a558c0fbcdf3ff438278a1
Puskar-Roy <puskarroy600@gmail.com>
2024-10-03 23:04:55 +0530

    feat : new

--- apps/frontend/src/components/Home/Home.jsx
+++ apps/frontend/src/components/Home/Home.jsx
@@ -3,7 +3,9 @@
 const Home = () => {
   return (
     <div className="text-4xl text-blue-500">
-      Hero
+      <h1 className="text-4xl text-rose-500">
+        Hey Puskar This Side
+      </h1>
     </div>
   )
 }
